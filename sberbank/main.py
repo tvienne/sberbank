20453e47e0739905cd3c2581bf1d39f894bb9927
Thibaud <vienne.thibaud@live.com>
2017-05-13 13:03:16 +0200

    first submission

--- sberbank/main.py
+++ sberbank/main.py
@@ -1,7 +1,8 @@
 import sys
 import os
 import pandas as pd
-import toolbox as tb
+import sberbank.toolbox as tb
+from sklearn.ensemble import RandomForestRegressor
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
@@ -31,4 +32,30 @@ full_df["radiation_raion"] = full_df["radiation_raion"].apply(lambda el: 1 if el
 full_df["railroad_1line"] = full_df["railroad_1line"].apply(lambda el: 1 if el == "yes" else 0)
 full_df["thermal_power_plant_raion"] = full_df["thermal_power_plant_raion"].apply(lambda el: 1 if el == "yes" else 0)
 
+# Clean some variable with mean
+full_df["full_sq"] = full_df["full_sq"].fillna(full_df["full_sq"].mean())
+full_df["life_sq"] = full_df["life_sq"].fillna(full_df["life_sq"].mean())
+full_df["floor"] = full_df["floor"].fillna(full_df["floor"].mean())
 
+# First dirty model
+features = ["full_sq", "life_sq"]
+label = "price_doc"
+
+# Train-val-test split
+train_val = full_df[full_df["test"] == 0]
+test = full_df[full_df["test"] == 1]
+x_train, x_val, y_train, y_val = tb.tt_split(train_val[features], train_val[label],  0.8)
+x_test = test[features]
+
+# Machine learning
+model = RandomForestRegressor(1000, max_depth=20)
+model.fit(x_train, y_train)
+pred_train = pd.Series(model.predict(x_train))
+pred_val = pd.Series(model.predict(x_val))
+pred_test = pd.Series(model.predict(x_test))
+print("Score Train : %s" % tb.rmsle(y_train, pred_train))
+print("Score Val : %s" % tb.rmsle(y_val, pred_val))
+
+# Export to Kaggle
+test["price_doc"] = pred_test
+tb.export_kaggle(test)
--- sberbank/toolbox.py
+++ sberbank/toolbox.py
@@ -1,5 +1,7 @@
 import math
+from sklearn.model_selection import train_test_split
 from time import gmtime, strftime
+import pandas as pd
 
 
 def rmsle(y, pred):
@@ -9,22 +11,37 @@ def rmsle(y, pred):
     :param pred: (pandas serie) prediction
     :return: rmsle
     """
-    log_y1 = math.log(y + 1)
-    log_pred1 = math.log(pred + 1)
-    square_log_serie = (log_y1 - log_pred1) * (log_y1 - log_pred1)
+    log_y1 = y.apply(lambda el: math.log(el + 1)).reset_index(drop=True)
+    log_pred1 = pred.apply(lambda el: math.log(el + 1))
+    square_log_serie = (log_y1 - log_pred1) ** 2
     result = math.sqrt(square_log_serie.mean())
     return result
 
 
-def export_kaggle(df, dir="../result/"):
+def export_kaggle(df, dir_path="../result/"):
     """
     Exports dataframe to kaggle format (columns id and price_doc)
     :param df: (pandas dataframe)
-    :param dir: (str) directory where to store file.
+    :param dir_path: (str) directory where to store file.
     :return: No return
     """
     if "price_doc" not in df.columns:
         raise ValueError("ERR : missing column price_doc. PLease make sure your prediction column name is 'price_doc'.")
 
     now = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
-    df[["id", "price_doc"]].to_csv(dir+"%s.csv" % now, sep=",")
+    df[["id", "price_doc"]].to_csv(dir_path+"%s.csv" % now, sep=",", index=False)
+
+
+def tt_split(x, y, test_size=0.8):
+    """
+    Train-Test split using sklearn
+    :param x: (pandas df)
+    :param y: (pandas serie)
+    :param test_size:
+    :return:
+    """
+    if not isinstance(x, pd.DataFrame):
+        raise ValueError("ERR : x has to be dataframe type.")
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
+    return x_train, x_test, y_train, y_test
