f791c37efaeb63a27318af41f698a20874714108
Krywyk <julien.krywyk@gmail.com>
2017-05-15 18:38:34 +0400

    Adding of the functions otherise_categorical_feature & get_dummies, for handling the categorical columns

--- sberbank/cleaning/categorical.py
+++ sberbank/cleaning/categorical.py
@@ -1,9 +1,10 @@
 """
 Deals with categorical features
 """
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-
-def yes_no_binarisation(df):
+def yes_no_binarisation(df,to_binarize):
     """
     @author : Thibaud
     Binarises Yes/no variables defined in variable $to_binarize.
@@ -11,12 +12,54 @@ def yes_no_binarisation(df):
     :return: dataframes with binarized features
     """
     # Binary categorical columns encoding :
-    to_binarize = ["water_1line", "big_market_raion", "big_road1_1line", "culture_objects_top_25",
-                   "detention_facility_raion", "incineration_raion", "detention_facility_raion",
-                   "nuclear_reactor_raion",
-                   "oil_chemistry_raion", "radiation_raion", "railroad_1line", "thermal_power_plant_raion"]
+    #to_binarize = ["water_1line", "big_market_raion", "big_road1_1line", "culture_objects_top_25",
+     #              "detention_facility_raion", "incineration_raion", "detention_facility_raion",
+     #              "nuclear_reactor_raion",
+     #              "oil_chemistry_raion", "radiation_raion", "railroad_1line", "thermal_power_plant_raion"]
     for col in to_binarize:
         df[col] = df[col].apply(lambda el: 1 if el == "yes" else 0)
 
     return df
 
+
+def otherise_categorical_feature(s, treshold):
+    """
+    @author : JK
+    Met à "others" les catégories qui apparaissent moins de treshold fois dans la série s
+    :param s: (pandas serie) 
+    :param treshold: le seuil d'occurences 
+    :return: s_copy, (pandas serie) avec des catégories à other.
+
+    """
+    nb_lignes_par_modalite = s.value_counts()
+    modalites_others = list(nb_lignes_par_modalite[nb_lignes_par_modalite < treshold].index)
+
+    s_copy = s.copy()
+    s_copy.ix[s_copy.isin(modalites_others)] = 'others'
+
+    return s_copy
+
+
+def get_dummies(col_categorical, full_df,treshold=100):
+    """
+    @author : JK
+    Créer des colones pour les variables catégorielles.
+    :param s: (pandas serie) 
+    :param treshold: le seuil d'occurences 
+    :return: s_copy, (pandas serie) avec des catégories à other.
+
+    """
+    print('initial full_df.shape :' , full_df.shape,'\n')
+    print('--- dummification of %s columns ...' %str(len(col_categorical)))
+    for var in col_categorical:
+        print('- ' + str(var))
+        cat_feature_otherised = otherise_categorical_feature(full_df[var],treshold)
+        cat_feature_otherised_dummified = pd.get_dummies(cat_feature_otherised, prefix=var)
+
+        full_df = pd.concat((full_df, cat_feature_otherised_dummified), axis=1)
+    print('\n--- temporary_shape  :%s' %str(full_df.shape))
+
+    # Drop categorical features
+    full_df.drop(col_categorical, axis=1, inplace=True)
+    print('--- droping ex-categorical columns :  %s' %str(full_df.shape))
+    return full_df
--- sberbank/cleaning/sq.py
+++ sberbank/cleaning/sq.py
@@ -1,7 +1,8 @@
 """
 Deals with sq features cleaning
 """
-
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
 
 def clean_sq(df):
     """
@@ -25,3 +26,64 @@ def clean_sq(df):
     return df
 
 
+def pred_nan_values(df):
+    """
+    @author : JK
+    Prédit les NaN d'une colonne en entraînant un model RandomForestRegressor sur les autres colonnes.
+    WARN : make sure that the line x_train = x_train.fillna(0) doesn't change the variable df ! 
+    TO DO : Verify this function
+    :param df: (pandas dataframe)
+    :return: df with the NaN values predicted.
+    """
+
+    ### Get the column having NaNs values.
+    nuls = []
+    for c in df.columns:
+        nuls.append(df[c].isnull().sum())
+
+    pct = [round((n * 100) / len(df), 0) for n in nuls]
+    df_nuls = pd.DataFrame({'col': df.columns, 'nan_count': nuls, '%': pct})
+    df_nuls = df_nuls.sort_values('nan_count', ascending=True)
+
+    col_full = df.columns
+
+    nan_count = df_nuls['nan_count'].tolist()
+    cols = df_nuls['col'].tolist()
+
+    print('--- NaN value prediction ---')
+    ### Do the trainings by beginning by the columns having the fewest amount of NaNs.
+    for i in range(len(df_nuls)):
+        if nan_count[i] != 0:
+            print(str(cols[i]) + ', ' + str(i) + '/' + str(len(nuls))+ ', '+ str(nan_count[i]) + ' Nan Values---')
+
+            ### clf initialization
+            clf_tmp = RandomForestRegressor(n_estimators=20, verbose=1, n_jobs=-1)
+
+            ### Train/test creation
+            col_tmp = list(set(col_full) - set([cols[i]]))
+            list_tmp = df[cols[i]].isnull()
+            df['tmp'] = list_tmp
+            x_train = df[df['tmp'] == False][col_tmp]
+            x_test = df[df['tmp'] == True][col_tmp]
+            y_train = df[df['tmp'] == False][cols[i]]
+            y_test = df[df['tmp'] == True][cols[i]]
+
+            df.drop('tmp', axis=1, inplace=True)
+
+            ### NaN values
+            x_train = x_train.fillna(0)
+            x_test = x_test.fillna(0)
+
+            clf_tmp.fit(x_train, y_train)
+            y_test_ = clf_tmp.predict(x_test)
+
+            ### Loop for replacing the NaN values by their predicted values
+            k = df[cols[i]].tolist()
+            k_tmp = list_tmp.tolist()
+            compteur = 0
+            for j in range(len(k_tmp)):
+                if k_tmp[j] == True:
+                    k[j] = y_test_[compteur]
+                    compteur = compteur + 1
+                    df[cols[i]] = k
+    return df,nuls
